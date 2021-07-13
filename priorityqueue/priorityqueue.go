4d8b95099af0e37fc4f231c0da9fa86cd39c9e81
Your Name <you@example.com>
2021-07-13 18:22:34 +0800

    init

--- priorityqueue/priorityqueue.go
+++ priorityqueue/priorityqueue.go
@@ -5,23 +5,23 @@ type item struct {
 	value    interface{}
 }
 
-type priorityqueue []item
+type PQueue []item
 
-func (pq priorityqueue) Len() int {
+func (pq PQueue) Len() int {
 	return len(pq)
 }
 
-func (pq priorityqueue) Less(i, j int) bool {
+func (pq PQueue) Less(i, j int) bool {
 	return pq[i].priority < pq[j].priority
 }
 
-func (pq priorityqueue) Swap(i, j int) {
+func (pq PQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 }
-func (pq *priorityqueue) Push(x interface{}) {
+func (pq *PQueue) Push(x interface{}) {
 	*pq = append(*pq, x.(item))
 }
-func (pq *priorityqueue) Pop() interface{} {
+func (pq *PQueue) Pop() interface{} {
 	temp := (*pq)[pq.Len()-1]
 	*pq = (*pq)[0 : pq.Len()-1]
 	return temp
